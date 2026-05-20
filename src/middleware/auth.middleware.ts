8d8fd1f7ab57e1b799223c073ebc1298ffd656d4
ashikurahman1 <ashikurahmantuhin@gmail.com>
2026-05-21 00:08:34 +0600

    feat: auth middleware

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,39 @@
+import type { NextFunction, Request, Response } from "express";
+import { sendError } from "../utils/response.js";
+import jwt from "jsonwebtoken";
+import { StatusCodes } from "http-status-codes";
+
+
+export interface AuthRequest extends Request {
+  user?: {
+    id: number;
+    name: string;
+    role: 'contributor' | 'maintainer'
+  }
+}
+
+export const authenticate = (req: AuthRequest, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
+
+  if(!authHeader){
+    return sendError(res, StatusCodes.UNAUTHORIZED, 'Authentication token missing');
+  }
+
+  try {
+    const decoded = jwt.verify(authHeader, process.env.JWT_SECRET as string) as any;
+    req.user = decoded;
+    next();
+    
+  } catch (error) {
+    return sendError(res, StatusCodes.UNAUTHORIZED, 'Invalid or expired token');
+  }
+}
+
+export const authorize = (roles: string[]) => {
+  return (req: AuthRequest, res: Response, next: NextFunction) => {
+    if(!req.user || !roles.includes(req.user.role)){
+      return sendError(res, StatusCodes.FORBIDDEN, 'Insufficient role permissions to access this resource');
+    }
+    next();
+  }
+}
